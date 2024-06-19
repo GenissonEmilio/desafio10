649afce51a226ea7dd28383358484225c1adeeda
GenissonEmilio <esgenisson@gmail.com>
2024-06-19 19:52:44 -0300

    update

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+function determinant(matriz) {
+  let c = matriz[0]
+  let c2 = matriz[1]
+  let de = c[0] * 
+  return de
+}
+
+const matrix = [
+[5,2,3],
+[4,1,3],
+[2,4,5]
+]
+
+console.log(determinant(matrix));
